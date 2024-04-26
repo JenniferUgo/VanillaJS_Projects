3e9d69bd897cad5540de2ee964f2a074f4c2251f
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2024-04-26 23:10:31 +0100

    Change background color

---
+++ 09-Dynamic Background Color/script.js
@@ -0,0 +1,7 @@
+let colorIndex = 0;
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'black', 'pink', 'brown',];
+
+document.getElementById('changeColorBtn').onclick = function() {
+    document.body.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+}
